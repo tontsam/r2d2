454ebeb500af3e17b7258db5c8070918a35921a2
tontsam <toni.musta@valossa.com>
2020-10-12 22:51:41 +0300

    sketch4

--- visu.py
+++ visu.py
@@ -4,7 +4,7 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import os
 import scipy.spatial.distance as scp
-
+import json
 
 def convert_to_codewords(vocab, name):
     asd = np.load(name)
@@ -17,17 +17,19 @@ def convert_to_codewords(vocab, name):
     tmp = vocab[np.argmin(dists, axis=1),:]
     return kp, tmp
 
-def image_detect_and_compute(img_name, vocab=[]):
+def image_detect_and_compute(img_name, vocab=[], method="r2d2"):
     img = cv.imread(img_name)
+    if not img_name[-5:] == ".r2d2":
+        img_name += ".r2d2"
     if not len(vocab):
-        data = np.load(img_name + ".r2d2")
+        data = np.load(img_name)
         return img, data["keypoints"], data["descriptors"]
-    kp, d = convert_to_codewords(vocab, img_name + ".r2d2")
+    kp, d = convert_to_codewords(vocab, img_name)
     return img, kp, d
 
-def run(path1, path2, vocab=[], draw=False):
+def search(img1, kp1, des1, path2, vocab=[], draw=False):
     
-    img1, kp1, des1 = image_detect_and_compute(path1, vocab=vocab)
+    #img1, kp1, des1 = image_detect_and_compute(path1, vocab=vocab)
     img2, kp2, des2 = image_detect_and_compute(path2, vocab=vocab)
     nmatches = 20
     #print(des1.dtype)
@@ -44,19 +46,32 @@ def run(path1, path2, vocab=[], draw=False):
     #plt.title(type(detector))
     plt.imshow(img_matches); plt.show()
 
-
-if __name__=="__main__":
-    #vocab = np.load("vocab.npy")
+def run(path):
+    tmp = {}
+    tmp["q"] = path
     vocab = []
     best_d = [np.inf for _ in range(5)]
     best_i = ["" for _ in range(5)]
-    for i in os.listdir("mmsys-b/test/"):
-        i = "mmsys-b/test/" + i
-        if i[-5:] == ".r2d2":
+    m = 0
+    #path = "q/landmarks-Query-100.jpg"
+    img1, kp1, des1 = image_detect_and_compute(path, vocab=vocab)
+    db = os.listdir("db/")
+    db = np.random.choice(db, 100)
+    path_ = path.split("/")[-1].split("-")
+    correct =  path_[0] + "-Reference-" + path_[-1]
+    if not correct in db:
+        db[0] = correct
+    for i in db:
+
+        i = "db/" + i
+        if not i[-5:] == ".r2d2":
             continue
-        
-        
-        d = run("mmsys-b/train/landmarks-100.jpg", i, vocab=vocab)
+        #if not "landmark" in i:
+        #    continue
+        m += 1
+        #if not "landmarks" in i:
+        #    continue
+        d = search(img1, kp1, des1, i, vocab=vocab)
         n = 0
         for j, d_ in enumerate(best_d):
         
@@ -69,4 +84,27 @@ if __name__=="__main__":
             
         
                 
-    print(best_d, best_i)
+    #print(best_d, best_i)
+    tmp["r"] = best_i
+    return tmp
+
+if __name__=="__main__":
+    results = []
+    result_path = "results.json"
+    if os.path.exists(result_path):
+        with open(result_path) as f:
+            results = json.load(f)
+    past_work = [x["q"] for x in results]    
+    for i in os.listdir("q/"):
+        i = "q/" + i
+        if i in past_work:
+            continue
+        if not i[-5:] == ".r2d2":
+            continue
+        if not "landmark" in i:
+            continue
+        results.append(run(i))
+        with open(result_path, "w") as f:
+            json.dump(results, f)
+
+    
